abc1af57ffc9b09e171a2877a9f147f772e73fc5
wangye <2736845454@qq.com>
2023-09-19 19:52:18 +0800

    commit919

---
+++ vue基础/19单文本组件/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <school></school>
+    <student></student>
+  </div>
+</template>
+
+<script>
+import School from "./School.vue";
+import Student from "./School.vue";
+
+export default {
+  name: "App",
+  components: {
+    School,
+    Student,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ vue基础/19单文本组件/School.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="demo">
+    <h1>学校名称:{{ school.name }}</h1>
+    <h1>学校地址:{{ school.address }}</h1>
+    <button @click="showName">点我提示学校名</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "School",
+  data() {
+    return {
+      school: {
+        name: "河南科技学院",
+        address: "河南省新乡市华兰大道",
+      },
+    }
+  },
+  methods: {
+    showName() {
+      console.log(this.school.name);
+    },
+  },
+};
+</script>
+
+<style>
+.demo {
+  background-color: orange;
+}
+</style>
+
+ 
